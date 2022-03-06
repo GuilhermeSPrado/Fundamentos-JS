809d4ff8801e02d901b4bb55cb0859d5f80565ed
ana <ana@hotmail.com>
2022-03-06 17:36:26 -0300

    Boolean

--- boolean.js
+++ boolean.js
@@ -1,8 +1,21 @@
 //retorna True ou False
-const primeiroNumero = 5;
+/*const primeiroNumero = 5;
 const segundoNumero = 10;
 let cadastroAtivado = false;
 
 const texto1='alura'
 const texto2='Alura'
-console.log(texto1==texto2)
+console.log(texto1==texto2)*/
+
+
+
+//Undefined
+/*let minhaVar;
+let varNull = null;
+console.log(minhaVar)
+console.log(varNull)*/
+
+let numero = 3
+let texto = 'string'
+console.log(typeof numero)
+console.log(typeof texto)
